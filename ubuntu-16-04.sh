cf09f53d2ecb1e42c30f19842eb1eb7360d3625d
Zhimin Wu <hoozecn@users.noreply.github.com>
2016-10-21 15:56:04 +0800

    create ubuntu:16.04

---
+++ ubuntu-16-04.sh
@@ -0,0 +1,76 @@
+apt-get update && apt-get install -y --no-install-recommends \
+		autoconf \
+		automake \
+		bzip2 \
+		file \
+		g++ \
+		gcc \
+		imagemagick \
+		libbz2-dev \
+		libc6-dev \
+		libcurl4-openssl-dev \
+		libdb-dev \
+		libevent-dev \
+		libffi-dev \
+		libgeoip-dev \
+		libglib2.0-dev \
+		libjpeg-dev \
+		libkrb5-dev \
+		liblzma-dev \
+		libmagickcore-dev \
+		libmagickwand-dev \
+		libmysqlclient-dev \
+		libncurses-dev \
+		libpng-dev \
+		libpq-dev \
+		libreadline-dev \
+		libsqlite3-dev \
+		libssl-dev \
+		libtool \
+		libwebp-dev \
+		libxml2-dev \
+		libxslt-dev \
+		libyaml-dev \
+		make \
+		patch \
+		xz-utils \
+		zlib1g-dev
+    
+apt-get install -y --no-install-recommends \
+		ca-certificates \
+		curl \
+		wget \
+    
+apt-get install -y --no-install-recommends \
+		bzr \
+		git \
+    git-extras \
+    git-flow \
+		mercurial \
+		openssh-client \
+		subversion \
+		procps
+    
+apt-get install -y --no-install-recommends vim\
+  dnsutils\
+  build-essential\
+  gzip\
+  unzip\
+  vim\
+  ruby-dev\
+  ruby-mkrf\
+  tmux\
+  tree\
+  python-dev\
+  python-pip\
+  rsync\
+  sudo\
+  lsof\
+  libssl-dev\
+  htop\
+  wget\
+  redis-tools\
+  less\
+  bc
+
+
